14b61fa26999c12ab935af58042ceddce8970ec2
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-24 22:40:50 +0100

    Adds variable prefix GLOBAL_ for global compilation flags

--- post_config.c_cpp.ext.mk
+++ post_config.c_cpp.ext.mk
@@ -1,6 +1,7 @@
 
-C_FLAGS ?=
-CPP_FLAGS ?=
+GLOBAL_C_FLAGS ?=
+GLOBAL_CPP_FLAGS ?=
+GLOBAL_C_CPP_FLAGS ?=
 
 CMD_COMPILOR_C = $(CMD_PREFIX)gcc -c -x c
 CMD_COMPILOR_CPP = $(CMD_PREFIX)g++ -c -x c++
@@ -8,10 +9,10 @@ CMD_COMPILOR_CPP = $(CMD_PREFIX)g++ -c -x c++
 $(BUILD_OBJ_DIR)%.c.o: $$(call rwildcard,./,*%.c) $$(MAKEFILE_LIST)
 	$(CMD_ECHO) "# c file <$<>"
 	$(CMD_MKDIR_ALL) $(BUILD_OBJ_DIR)
-	$(CMD_COMPILOR_C) $(C_FLAGS) $(FLAGS) -o $@ -MMD -MF $(patsubst %.o,%.d, $@) $<
+	$(CMD_COMPILOR_C) $(GLOBAL_C_FLAGS) $(GLOBAL_C_CPP_FLAGS) $(C_FLAGS) $(C_CPP_FLAGS) -o $@ -MMD -MF $(patsubst %.o,%.d, $@) $<
 
 $(BUILD_OBJ_DIR)%.cpp.o: $$(call rwildcard,./,*%.cpp) $$(MAKEFILE_LIST)
 	$(CMD_ECHO) "# c++ file <$<>"
 	$(CMD_MKDIR_ALL) $(BUILD_OBJ_DIR)
-	$(CMD_COMPILOR_CPP) $(CPP_FLAGS) $(FLAGS) -o $@ -MMD -MF $(patsubst %.o,%.d, $@) $<
+	$(CMD_COMPILOR_CPP) $(GLOBAL_CPP_FLAGS) $(GLOBAL_C_CPP_FLAGS) $(CPP_FLAGS) $(C_CPP_FLAGS) -o $@ -MMD -MF $(patsubst %.o,%.d, $@) $<
 
--- post_config.s.ext.mk
+++ post_config.s.ext.mk
@@ -1,10 +1,10 @@
 
-S_FLAGS ?=
+GLOBAL_S_FLAGS ?=
 
 CMD_COMPILOR_S = $(CMD_PREFIX)gcc -c -x assembler
 
 $(BUILD_DIR)%.s.o: $$(call rwildcard,./,*%.s) $(MAKEFILE_LIST)
 	$(CMD_ECHO) "# asm file <$<>"
 	$(CMD_MKDIR_ALL) $(BUILD_OBJ_DIR)
-	$(CMD_COMPILOR_S) $(S_FLAGS) $(FLAGS) $< -o $@
+	$(CMD_COMPILOR_S) $(GLOBAL_S_FLAGS) $(S_FLAGS) $< -o $@
 
