260357794672cff8a0136b9084571951a7248b5b
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-25 17:44:03 +0100

    Adds the makefile repository directory .mkrepo and implements 'make set config=CONFIG_NAME'

--- build.mk
+++ build.mk
@@ -23,7 +23,7 @@ GLOBAL_UPDATE_TARGETS =
 
 #------------------------------------------------------------------------------- COMMANDS SYMBOLS
 
-CMD_PREFIX ?=
+CMD_PREFIX ?= @
 
 CMD_CP = $(CMD_PREFIX)cp
 CMD_ECHO = @echo
@@ -32,10 +32,31 @@ CMD_MKDIR_ALL = $(CMD_MKDIR) -p
 CMD_RM = $(CMD_PREFIX)rm
 
 
-#------------------------------------------------------------------------------- PRE-CONFIG INIT
+#------------------------------------------------------------------------------- PROJECT DIR
 
 PROJECT_DIR ?= ./
-BUILD_DIR ?= $(PROJECT_DIR)build/
+
+
+#------------------------------------------------------------------------------- MK REPOSITORY
+
+MK_REPOSITORY_DIR = $(lastword $(call url_find_parent,$(PROJECT_DIR),.mkrepo/))
+
+MK_SELECTED_CONFIG = default_config
+
+ifeq ($(MK_REPOSITORY_DIR),)
+    MK_REPOSITORY_DIR = $(PROJECT_DIR).mkrepo/
+else
+    MK_SELECTED_CONFIG = $(shell cat "$(MK_REPOSITORY_DIR)selected_config")
+endif
+
+ifdef config
+    MK_SELECTED_CONFIG = $(config)
+endif
+
+
+#------------------------------------------------------------------------------- COMMANDS
+
+BUILD_DIR ?= $(PROJECT_DIR)build-$(MK_SELECTED_CONFIG)/
 BUILD_PRODUCT_DIR ?= $(BUILD_DIR)product/
 
 
@@ -52,6 +73,10 @@ clean:
 
 full: clean update
 
+set:
+	$(CMD_MKDIR_ALL) $(MK_REPOSITORY_DIR)
+	$(CMD_ECHO) $(MK_SELECTED_CONFIG) > $(MK_REPOSITORY_DIR)selected_config
+
 
 #------------------------------------------------------------------------------- PRE-CONFIG EXTENSIONS
 
@@ -60,7 +85,13 @@ include $(call rwildcard,$(MK_DIR),pre_config.*.ext.mk)
 
 #------------------------------------------------------------------------------- CONFIG
 
--include default_config.mk
+MK_CONFIG_PATH = $(MK_SELECTED_CONFIG).mk
+
+ifeq ($(wildcard $(MK_CONFIG_PATH)),)
+    $(error $(MK_CONFIG_PATH) does not exist)
+else
+    include $(MK_CONFIG_PATH)
+endif
 
 
 #------------------------------------------------------------------------------- POST-CONFIG EXTENSIONS
--- readme.txt
+++ readme.txt
@@ -1,6 +1,10 @@
 
 Guillaume Abadie's makefile solution
 
+@parameters:
+    - config=CONFIG_NAME
+        specify the CONFIG_NAME.mk configuration file in the
+        project's directory
 
 @common commands:
     > make
@@ -13,4 +17,7 @@ Guillaume Abadie's makefile solution
     > make full
     > make clean ; make
         Force compilation from scratch
+
+    > make set
+        Saves modified variable into the project
 
