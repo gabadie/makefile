aa244cc92a46a52dd36ed1c85da308efac97c7b1
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-25 19:59:35 +0100

    Implements mkrepo_fetch_param returning a parameter's value in the repo

--- repository.mk
+++ repository.mk
@@ -6,6 +6,16 @@ ifeq ($(MK_REPOSITORY_DIR),)
 endif
 
 #
+# @infos: Fetches param's value from the repository
+#
+# @uses:
+#    $(call mkrepo_fetch_param,PARAMETER_NAME)
+#
+# @param <PARAMETER_NAME>: parameter's name
+#
+mkrepo_fetch_param = $(shell cat "$(MK_REPOSITORY_DIR)$1" 2> /dev/null)
+
+#
 # @infos: Load param if not already seted, specifying a default value in cas
 #
 # @uses:
@@ -14,5 +24,5 @@ endif
 # @param <PARAMETER_NAME>: parameter's name
 # @param <DEFAULT_VALUE>: parameter's default value
 #
-mkrepo_load_param = $($(eval TMP_VALUE := $(shell cat "$(MK_REPOSITORY_DIR)$1" 2> /dev/null))$(if $(TMP_VALUE),$(eval $1 ?= $(TMP_VALUE)),$(eval $1 := $2)))
+mkrepo_load_param = $($(eval TMP_VALUE := $(call mkrepo_fetch_param,$1))$(if $(TMP_VALUE),$(eval $1 ?= $(TMP_VALUE)),$(eval $1 := $2)))
 
