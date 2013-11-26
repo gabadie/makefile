164dbd5c6dc6832bd951c826ba4a492904544500
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-26 17:55:38 +0100

    Fixes burning error while compiling static library for the first time

--- post_config.staticlib.ext.mk
+++ post_config.staticlib.ext.mk
@@ -6,6 +6,6 @@ CMD_LINK_LIB = $(CMD_PREFIX)ar -rcs
 $(STATICLIB_TARGETS): %:
 	$(CMD_ECHO) "# linking static lib <$@>"
 	$(CMD_MKDIR_ALL) $(dir $@)
-	$(CMD_RM) $@
+	$(CMD_RM) -f $@
 	$(CMD_LINK_LIB) $@ $^
 
