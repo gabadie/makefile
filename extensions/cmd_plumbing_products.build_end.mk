1c4d7f997f6d7f1c4cb835b934b1e398573bfb25
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 14:22:00 +0100

    Removes project_products function

--- extensions/cmd_plumbing_products.build_end.mk
+++
@@ -1,9 +0,0 @@
-
-PLUMBING_PRODUCTS = $(PLUMBING_PREFIX)products
-
-.PHONY: $(PLUMBING_PRODUCTS)
-$(PLUMBING_PRODUCTS):
-	@echo > /dev/null; $(foreach PRODUCT,$(PROJECT_PRODUCTS_SELECTED),\
-            echo "$(call product_target,$(PRODUCT))";\
-        )
-
--- extensions/external_project.main_config_pre.mk
+++ extensions/external_project.main_config_pre.mk
@@ -1,19 +1,5 @@
 
 #
-# @infos: Gets project's product
-#
-# @uses:
-#   $(call project_products,$(EXTERN_PROJECT_DIR))
-#   $(call project_products,$(EXTERN_PROJECT_DIR),$(OPTIONS))
-#   $(call project_products,$(EXTERN_PROJECT_DIR),$(OPTIONS),$(SPECIFIC_PRODUCTS))
-#
-# @param EXTERN_PROJECT_DIR: external project's dir
-# @param OPTIONS: options (exemple: config=release)
-# @param SPECIFIC_PRODUCTS: product list filter (exemple: product_a product_b ...)
-#
-project_products = $(addprefix $1,$(shell make -C $1 plumbing_products $2 $(if $3,"projects=$3")))
-
-#
 # @uses:
 #   $(call project_product,$(EXTERN_PROJECT_DIR),$(PRODUCT_NAME))
 #   $(call project_product,$(EXTERN_PROJECT_DIR),$(PRODUCT_NAME),$(OPTIONS))
