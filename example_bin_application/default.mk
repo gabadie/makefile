bf05981515362f17e627b882bbcd88d8692bdbeb
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 14:12:03 +0100

    Lets example_bin_application/ compile example_bin_staticlib in example_bin_staticlib/

--- example_bin_application/default.mk
+++ example_bin_application/default.mk
@@ -1,4 +1,7 @@
 
+HELLO_WORLD_LIB := $(call project_product,../example_bin_staticlib/,example_bin_staticlib)
+
+GLOBAL_DEPENDENCIES := $(call product_target,$(HELLO_WORLD_LIB))
 GLOBAL_CPP_FLAGS := $(call project_libheaders_dir,../example_bin_staticlib/)
 GLOBAL_LINK_FLAGS := $(call project_products,../example_bin_staticlib/)
 
