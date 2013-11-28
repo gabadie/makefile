4807b18f8b111cb6c2708a836db31ca13b37f85c
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 17:33:45 +0100

    Removes config_templates/bin_application.mk

--- example_bin_application/default.mk
+++ example_bin_application/default.mk
@@ -4,5 +4,5 @@ HELLO_WORLD_LIB := $(call project_product,../example_bin_staticlib/,example_bin_
 GLOBAL_DEPENDENCIES := $(call product_target,$(HELLO_WORLD_LIB))
 GLOBAL_CPP_FLAGS := $(call project_libheaders_dir,../example_bin_staticlib/)
 
-include ../config_templates/bin_application.mk
+include ../config_templates/bin_common.mk
 
