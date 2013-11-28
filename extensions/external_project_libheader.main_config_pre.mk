30d4f69e2c402789764b0fead8d81ea568291e89
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 19:51:08 +0100

    Renames 'make plumbing_include' to 'make plumbing_build_include_dir'

--- extensions/external_project_libheader.main_config_pre.mk
+++ extensions/external_project_libheader.main_config_pre.mk
@@ -1,5 +1,5 @@
 
-PLUMBING_INCLUDE = $(PLUMBING_PREFIX)include
+PLUMBING_BUILD_INCLUDE_DIR = $(PLUMBING_PREFIX)build_include_dir
 
 #
 # @infos: Gets project's lib headers directory
@@ -11,9 +11,9 @@ PLUMBING_INCLUDE = $(PLUMBING_PREFIX)include
 # @param EXTERN_PROJECT_DIR: external project's dir
 # @param OPTIONS: options (exemple: config=release)
 #
-project_libheaders_dir = $(addprefix -I $1,$(shell make -C $1 $(PLUMBING_INCLUDE) $2))
+project_libheaders_dir = $(addprefix -I $1,$(shell make -C $1 $(PLUMBING_BUILD_INCLUDE_DIR) $2))
 
-.PHONY: $(PLUMBING_INCLUDE)
-$(PLUMBING_INCLUDE):
+.PHONY: $(PLUMBING_BUILD_INCLUDE_DIR)
+$(PLUMBING_BUILD_INCLUDE_DIR):
 	@echo $(BUILD_INCLUDE_DIR)
 
