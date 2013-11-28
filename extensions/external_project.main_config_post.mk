e075cfa10b4ca203be236661e98c77ca707a133f
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 14:17:32 +0100

    Shows product name and project dir when makeing recusrively

--- extensions/external_project.main_config_post.mk
+++ extensions/external_project.main_config_post.mk
@@ -3,6 +3,6 @@ EXTERNAL_TARGETS := $(foreach PROD,$(EXTERNAL_PRODUCTS), $(call product_target,$
 
 .PHONY: $(EXTERNAL_TARGETS)
 $(EXTERNAL_TARGETS): %:
-	$(CMD_ECHO) "# external product ..."
+	$(CMD_ECHO) "# external product <$(EXTERNAL_PROJECT_PRODUCT)> (in project $(EXTERNAL_PROJECT_DIR))"
 	$(CMD_PREFIX)make -C $(EXTERNAL_PROJECT_DIR) $(EXTERNAL_PROJECT_OPTIONS) products=$(EXTERNAL_PROJECT_PRODUCT)
 
