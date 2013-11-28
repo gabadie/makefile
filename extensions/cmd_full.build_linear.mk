4b0f27035d5bea703bf8a003b81a88365eb54f06
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 15:59:11 +0100

    Implements 'make full/rec' recursively compiling from scratch

--- extensions/cmd_full.build_linear.mk
+++ extensions/cmd_full.build_linear.mk
@@ -1,6 +1,8 @@
 
-.PHONY: full
-full: clean $$(PROJECT_TARGETS)
+.PHONY: full full/rec
+full/rec: EXTERNAL_REC_OPTIONS = full/rec
+
+full full/rec: clean $$(PROJECT_TARGETS)
 	$(CMD_MESSAGE) "builds from scratsh finished"
 
 include $(extension_entry)
--- extensions/external_project.main_config_post.mk
+++ extensions/external_project.main_config_post.mk
@@ -4,6 +4,6 @@ EXTERNAL_TARGETS := $(foreach PROD,$(EXTERNAL_PRODUCTS), $(call product_target,$
 .PHONY: $(EXTERNAL_TARGETS)
 $(EXTERNAL_TARGETS): %:
 	$(CMD_MESSAGE) "external product <$(EXTERNAL_PROJECT_PRODUCT)> (in project $(EXTERNAL_PROJECT_DIR))"
-	$(CMD_PREFIX)make -C $(EXTERNAL_PROJECT_DIR) $(EXTERNAL_PROJECT_OPTIONS) products=$(EXTERNAL_PROJECT_PRODUCT) "message_indent=$(message_indent) ."
+	$(CMD_PREFIX)make -C $(EXTERNAL_PROJECT_DIR) $(EXTERNAL_PROJECT_OPTIONS) $(EXTERNAL_REC_OPTIONS) products=$(EXTERNAL_PROJECT_PRODUCT) "message_indent=$(message_indent) ."
 	$(CMD_MESSAGE)
 
