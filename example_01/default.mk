45b470e799ecb62b63d69fe59ae7d47462464a66
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 12:54:47 +0100

    Updates example_01/

--- example_01/default.mk
+++ example_01/default.mk
@@ -1,9 +1,6 @@
 
-EXAMPLE_TARGET = $(BUILD_PRODUCT_DIR)raytracer
-
-EXEC_TARGETS += $(EXAMPLE_TARGET)
-
-$(EXAMPLE_TARGET): $$(call o_files,$(call rfindall,cpp))
+EXAMPLE_PRODUCT := $(call product_create,EXEC,opencl_demo)
+EXAMPLE_TARGET = $(call product_target,$(EXAMPLE_PRODUCT))
 
 ifeq ($(OS_NAME),Darwin)
     OPENCL_LINK_FLAG = -framework OpenCL
@@ -11,5 +8,6 @@ else
     OPENCL_LINK_FLAG = -lopencl
 endif
 
+$(EXAMPLE_TARGET): $$(call o_files,$(call rfindall,cpp))
 $(EXAMPLE_TARGET): LINK_EXEC_FLAGS = $(OPENCL_LINK_FLAG)
 
