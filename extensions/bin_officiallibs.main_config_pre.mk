d1a30770bffc8f1fb5176cb2152c8a1b1b2ef59b
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-28 18:23:52 +0100

    Implements bin_officiallibs function to get linking flags for several official libraries at the same time

--- extensions/bin_officiallibs.main_config_pre.mk
+++ extensions/bin_officiallibs.main_config_pre.mk
@@ -10,6 +10,17 @@
 #
 bin_officiallib = $(BIN_OFFICIALLIB_$(strip $1))
 
+#
+# @infos: get link flags for several offcial libraries
+#
+# @uses:
+#   BINEXEC_FLAGS += $(call bin_officiallibs,$(LIB_NAMES))
+#
+# @example:
+#   BINEXEC_FLAGS += $(call bin_officiallibs,opencl opengl)
+#
+bin_officiallibs = $(foreach LIB_NAME,$1,$(call bin_officiallib,$(LIB_NAME)))
+
 
 BIN_OFFICIALLIB_opencl = $(if ($(OS_NAME),Darwin), -framework OpenCL, -lopencl)
 
