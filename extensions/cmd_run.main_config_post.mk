4f995cb4605fa08011fdf78734abb58d21bb16bb
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-30 13:55:46 +0100

    Implements 'make run/product/<product_name>'

---
+++ extensions/cmd_run.main_config_post.mk
@@ -0,0 +1,17 @@
+
+#
+# make run/product/<product_name>
+#
+
+RUN_RULES = $(addprefix run/product/,$(RUN_PRODUCTS))
+
+.PHONY: $(RUN_RULES)
+
+$(foreach PRODUCT,$(RUN_PRODUCTS), \
+    $(eval run/product/$(PRODUCT): $(call product_target,$(PRODUCT))) \
+)
+
+$(RUN_RULES):
+	$(CMD_MESSAGE) "running <$<>"
+	$(CMD_PREFIX)./$<
+
