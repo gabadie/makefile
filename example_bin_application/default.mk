6454a4ade2d7c2b28398d04d05d8d856391331b2
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-01 16:33:47 +0100

    mv config_templates/ -> quick_configs/

--- example_bin_application/default.mk
+++ example_bin_application/default.mk
@@ -4,5 +4,5 @@ HELLO_WORLD_LIB := $(call project_product,../example_bin_staticlib/,example_bin_
 GLOBAL_DEPENDENCIES := $(call product_target,$(HELLO_WORLD_LIB))
 GLOBAL_CPP_FLAGS := $(call project_build_include_dir,../example_bin_staticlib/)
 
-include ../config_templates/bin_common.mk
+include ../quick_configs/bin_common.mk
 
