7851b3551993822c567efd6e86590727bf783774
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-01 15:34:59 +0100

    Implements bin_object_file

--- extensions/bin_o_files.main_config_pre.mk
+++ extensions/bin_o_files.main_config_pre.mk
@@ -2,10 +2,18 @@
 BUILD_OBJ_DIR ?= $(BUILD_DIR)objects/
 
 #
+# @infos: Gets object file from source file
+#
+# @example:
+#   $(call bin_object_file,main.cpp)
+#
+bin_object_file = $(BUILD_OBJ_DIR)$(strip $(notdir $1)).o
+
+#
 # @infos: Gets object files from source files
 #
 # @example:
 #   $(call bin_object_files,main.cpp hello.c boot.s)
 #
-bin_object_files = $(addprefix $(BUILD_OBJ_DIR),$(addsuffix .o,$(notdir $1)))
+bin_object_files = $(foreach FILE,$1,$(call bin_object_file,$(FILE)))
 
