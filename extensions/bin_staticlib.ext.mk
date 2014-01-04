70a9945f75d94ea6eba1c84683f20025899e168e
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-04 22:26:09 +0100

    Migartes bin_staticlib extension

--- extensions/bin_staticlib.ext.mk
+++ extensions/bin_staticlib.ext.mk
@@ -1,4 +1,12 @@
 
+ifeq ($(extension_entry),/config/pre)
+
+BINLIBSTATIC_EXTENSION = .a
+
+endif
+
+ifeq ($(extension_entry),/config/post)
+
 BINLIBSTATIC_TARGETS := $(foreach PROD,$(BINLIBSTATIC_PRODUCTS), $(call product_target,$(PROD)))
 BINLIBSTATIC_CMD = $(CMD_PREFIX)ar -rcs
 
@@ -9,4 +17,6 @@ $(BINLIBSTATIC_TARGETS): %:
 	$(CMD_MKDIR_ALL) $(dir $@)
 	$(CMD_RM) -f $@
 	$(BINLIBSTATIC_CMD) $@ $^
+
+endif
 
