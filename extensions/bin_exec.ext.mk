b7b335fceb08a61141f3dd29c1b2480bb71848ab
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-04 22:04:17 +0100

    Migrates bin_exec extension

--- extensions/bin_exec.ext.mk
+++ extensions/bin_exec.ext.mk
@@ -1,4 +1,13 @@
 
+ifeq ($(extension_entry),/config/pre)
+
+BINEXEC_EXTENSION =
+RUN_PRODUCTS += $(BINEXEC_PRODUCTS)
+
+endif
+
+ifeq ($(extension_entry),/config/post)
+
 BINEXEC_TARGETS := $(foreach PROD,$(BINEXEC_PRODUCTS), $(call product_target,$(PROD)))
 BINEXEC_CMD = $(CMD_PREFIX)g++
 
@@ -8,4 +17,6 @@ $(BINEXEC_TARGETS):
 	$(CMD_MESSAGE) "linking executable <$@>"
 	$(CMD_MKDIR_ALL) $(dir $@)
 	$(BINEXEC_CMD) $(BINEXEC_FLAGS) -o $@ $(filter %.o %.a %.c %.cpp %.s,$^)
+
+endif
 
