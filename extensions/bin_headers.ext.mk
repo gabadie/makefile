6fa1f87437ecffed7bfd345a525ac362511c7db3
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-04 22:12:55 +0100

    Migrates bin_headers extension

--- extensions/bin_headers.ext.mk
+++ extensions/bin_headers.ext.mk
@@ -1,4 +1,20 @@
 
+ifeq ($(extension_entry),/config/pre)
+
+#
+# @infos: return header dependencies
+#
+# @uses:
+#   $(call bin_header_deps,$(MY_HEADER))
+#
+# @param <MY_HEADER>: path to the header file
+#
+bin_header_deps = $(filter-out /* ../* : \,$(shell g++ -E -x c++-header -MM -MT "" "$1"))
+
+endif
+
+ifeq ($(extension_entry),/config/post)
+
 BINHEADERS_TARGETS := $(foreach PROD,$(BINHEADERS_PRODUCTS), $(call product_target,$(PROD)))
 BINHEADERS_CMD = $(CMD_PREFIX)ar -rcs
 
@@ -13,4 +29,6 @@ $(BINHEADERS_TARGETS): %:
             rm -rf $@ ; \
             exit 1; \
         fi;
+
+endif
 
