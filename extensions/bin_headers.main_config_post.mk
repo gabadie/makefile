dd3ca0df67a97fa2e404a2074cca0cbade0099c2
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-04 20:57:46 +0100

    Removes binary headers' directory when failed

--- extensions/bin_headers.main_config_post.mk
+++ extensions/bin_headers.main_config_post.mk
@@ -4,9 +4,13 @@ BINHEADERS_CMD = $(CMD_PREFIX)ar -rcs
 
 PROJECT_TARGETS += $(BINHEADERS_TARGETS)
 
-$(BINHEADERS_TARGETS):
+$(BINHEADERS_TARGETS): %:
 	$(CMD_MESSAGE) "headers directory <$@/>"
 	$(CMD_RM) -rf $@
 	$(CMD_MKDIR_ALL) $@/
-	$(CMD_CP) $^ $@
+	$(CMD_CP) $^ $@ ;\
+        if [ $$? -ne 0 ]; then \
+            rm -rf $@ ; \
+            exit 1; \
+        fi;
 
