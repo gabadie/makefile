eba17deb4df83d8acc58dfb5e4a610855d243c93
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-04 22:22:07 +0100

    Migrates bin_s_files extension

--- extensions/bin_s_files.ext.mk
+++ extensions/bin_s_files.ext.mk
@@ -1,8 +1,12 @@
 
+ifeq ($(extension_entry),/config/pre)
+
 CMD_COMPILOR_S = $(CMD_PREFIX)gcc -c -x assembler
 
 $(BUILD_DIR)%.s.o: $$(call rwildcard,./,*%.s) $(MAKEFILE_LIST)
 	$(CMD_MESSAGE) "asm file <$<>"
 	$(CMD_MKDIR_ALL) $(BUILD_OBJ_DIR)
 	$(CMD_COMPILOR_S) $(S_FLAGS) $< -o $@
+
+endif
 
