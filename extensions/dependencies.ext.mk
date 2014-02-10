f26a29c054dd64106f2befc1773e6b0b7f154833
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-10 18:36:15 +0100

    Fixes dependencies bug

--- extensions/dependencies.ext.mk
+++ extensions/dependencies.ext.mk
@@ -3,6 +3,10 @@ ifeq ($(extension_entry),/config/pre)
 
 BUILD_DEPS_DIR ?= $(BUILD_DIR)dependencies/
 
+endif
+
+ifeq ($(extension_entry),/config/post)
+
 -include $(call rwildcard,$(BUILD_DEPS_DIR),*.d)
 
 endif
