2377d00e0144991edac321253271090720bba98b
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-12 15:51:21 +0100

    Fixes circular dependencies caused by including .d files with targets depending on the included makefiles

--- extensions/dependencies.ext.mk
+++ extensions/dependencies.ext.mk
@@ -7,7 +7,18 @@ endif
 
 ifeq ($(extension_entry),/config/post)
 
--include $(call rwildcard,$(BUILD_DEPS_DIR),*.d)
+DEPENDENCY_FILES := $(abspath $(call rwildcard,$(BUILD_DEPS_DIR),*.d))
+
+-include $(DEPENDENCY_FILES)
+
+#
+# Depency files are only files containing data. They don't have any makefile algorithms. Therefore we filter out them
+# from MAKEFILE_LIST used in MK_DEPENDENCIES to avoid circular dependencies. Elsewhere it causes a bug rebuilding the
+# whole project when two rules generating dependency files are built not excatly at the very same second.
+#
+$(eval MK_DEPENDENCIES_WITH_D_FILES := $(value MK_DEPENDENCIES))
+
+MK_DEPENDENCIES = $(filter-out $(DEPENDENCY_FILES),$(abspath $(MK_DEPENDENCIES_WITH_D_FILES)))
+
 
 endif
-
