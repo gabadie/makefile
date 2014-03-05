4055d3c02a00c1bbebb6674375648670a5ac76da
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-05 13:01:24 +0100

    Lets test_script function print the running environment

--- extensions/test_script.ext.mk
+++ extensions/test_script.ext.mk
@@ -23,7 +23,7 @@ test_script = $(call test_script_log_path,$1) \
     $(eval _TEST_SCRIPT_LOG_TARGETS += $(call test_script_log_path,$1)) \
     $(eval $(call test_script_log_path,$1): $1) \
     $(eval $(call test_script_log_path,$1): TEST_SCRIPT_PATH = $1) \
-    $(eval $(call test_script_log_path,$1): TEST_SCRIPT_ENV = $2)
+    $(eval $(call test_script_log_path,$1): TEST_SCRIPT_ENV = $(call run_guess_env,$1,$2))
 
 #
 # @infos: Defines test scripts and returns their targets
@@ -46,11 +46,11 @@ _TEST_LOG_TARGETS += $(_TEST_SCRIPT_LOG_TARGETS)
 
 $(_TEST_SCRIPT_LOG_TARGETS): %: $$(MK_DEPENDENCIES)
 	$(CMD_MKDIR_ALL) $(dir $@)
-	$(call history_colored_rule,testing script,$(TEST_SCRIPT_PATH),GREEN)
+	$(call history_colored_rule,testing $(strip $(TEST_SCRIPT_ENV)) script,$(TEST_SCRIPT_PATH),GREEN)
 	$(CMD_PREFIX)$(call run_script_cmd,$(TEST_SCRIPT_PATH), $(TEST_SCRIPT_ENV)) $(TESTFLAGS) > $@ ; \
 		RETURN=$$? ; \
         if [ $$RETURN -ne 0 ]; then \
-            echo "$(call color_error)<$<> has returned $$RETURN$(call color_reset)"; \
+            echo "$(call color_error)<$(TEST_SCRIPT_PATH)> has returned $$RETURN$(call color_reset)"; \
             cat $@ ; \
             rm -rf $@ ; \
             echo ; \
